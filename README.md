b1901b4ba6cab396830d0f651b851f0b3e730d3e
basuabhirup <basu.abhirup@yahoo.com>
2021-07-28 13:12:51 +0530

    Completed Day 8 Project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Day 8 Challenge: HTML 5 Canvas
+
+#### Live Site URL: https://basuabhirup.github.io/js30-day-08-html5-canvas/
+
+This is the project for the Day 7 of the 30-days vanilla JavaScript coding challenge from https://javascript30.com/
+
+
+[!Screenshot of the completed Project](./screenshot-day08.png)
---
+++ index.js
@@ -0,0 +1,45 @@
+const canvas = document.querySelector("#draw");
+canvas.style.backgroundColor = "#000";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// creating a variable to use the context:
+const ctx = canvas.getContext("2d");
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+ctx.lineWidth = 2;
+
+let lastX, lastY;
+let hue = 0;
+let lineGrow = true;
+let toDraw = false;
+
+// Defining a custom draw function:
+const draw = (event) => {
+  if (!toDraw) return; // making sure that the mouse is down
+
+  console.log(ctx.lineWidth);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath(); // this particular call will implement different colours and widths than the precvious
+  ctx.moveTo(lastX, lastY); // values from the immediately previous call
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+
+  hue >= 360 ? hue = 0 : hue ++ ; // ternary operator to range bound the hue value
+
+  (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) ? lineGrow = !lineGrow : lineGrow = lineGrow;
+  lineGrow ? ctx.lineWidth ++ : ctx.lineWidth -- ;
+
+  [lastX, lastY] = [event.offsetX, event.offsetY]; // values to be used in the immediately next call
+}
+
+// Adding event listeners on the canvas element:
+canvas.addEventListener("mousedown", (e) => {
+  toDraw = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // setting the inital values
+  draw(e);
+});
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => toDraw = false); // withdraws the painter
+canvas.addEventListener("mouseout", () => toDraw = false);
